7e8907f6fbe9519e75a85bf0b1f694bdb4a89568
Alar <alar@hkhk.edu.ee>
2024-10-04 11:41:38 +0300

    Add files via upload

---
+++ 02 pooleli.py
@@ -0,0 +1,14 @@
+# Alar Tammes
+# 04.10.24
+# Harjutus 2
+
+# 9. arvusüsteem
+arv = int(input("Lisa 10nd arv: "))
+print(bin(arv))
+print(hex(arv))
+
+#10.Kütusekulu arvutamine
+liiter = int(input("Lisa kütusekulu: "))
+km = int(input("Lisa läbitus kilomeetrid:" ))
+kytusekulu = liiter/(km/100)
+print("Sinu keskmine kütusekulu on: ", kytusekulu)
