5a8f51a8af293ab510363938c926f7c8ab7e7f3d
Alar <alar@hkhk.edu.ee>
2024-10-04 11:41:49 +0300

    Add files via upload

---
+++ sissejuhtaus.py
@@ -0,0 +1,18 @@
+# Alar Tammes
+# 04.10.24
+# Harjutus 1
+
+print('"Cabernet Sauvignon" või \'Cabernet Sauvignon\'?')
+print('  @..@')
+print(' (----)')
+print('( \__/ )')
+print('^^ "" ^^')
+print()
+
+#int täisarv 123
+#str sõne "tekst"
+#float komaartv 1.23
+
+a = int(input("Lisa ruudu külje pikkus: "))
+s = a ** 2 #astendamine
+print("Vastus: "'ruudu pindala on ',s)
