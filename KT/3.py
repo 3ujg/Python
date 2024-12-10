25076e0e7ef0c4905ced0f202b54e6dbc39c4d17
Alar <alar@hkhk.edu.ee>
2024-12-10 09:57:32 +0200

    asdas

---
+++ KT/3.py
@@ -0,0 +1,10 @@
+#4.1
+#banner = input("Mis banneriks valisid: ")
+#k = int(input("Mitu korda soovid printida: "))
+#for i in range (k):
+#    print(str.upper(banner))
+
+#4.2
+õunte_kogus = int(input("Mitu õuna on: "))
+mahlapakkida_arv = (õunte_kogus*0,4/3)
+print(mahlapakkida_arv)
