81f93c6d571dfe569c3e95e2c8250d8c25008a93
Alar <alar@hkhk.edu.ee>
2024-12-12 11:06:41 +0200

    Update 3.py

--- KT/3.py
+++ KT/3.py
@@ -5,6 +5,13 @@
 #    print(str.upper(banner))
 
 #4.2
-õunte_kogus = int(input("Mitu õuna on: "))
-mahlapakkida_arv = (õunte_kogus*0,4/3)
-print(mahlapakkida_arv)
+# õunte_kogus = int(input("Mitu õuna on: "))
+# mahlapakkide_arv = round(õunte_kogus*0.4/3)
+# print(f"Mahla on {mahlapakkide_arv} liitrit")
+
+#4.3
+Kutsutud_inimesed = int(input("Kui palju inimesi on kutsutud?: "))
+inimesed_tulevad = int(input("Kui palju inimesi tuleb?: "))
+maksimaalne_eelarve = Kutsutud_inimesed * 10 + 55
+minimaalne_eelarve = inimesed_tulevad * 10 + 55
+print(f"{Kutsutud_inimesed} Inimest on kutsutud\n{inimesed_tulevad} Inimest tuleb\n{maksimaalne_eelarve} on maksimaalne eelarve\n{minimaalne_eelarve} on minimaalne eelarve")
