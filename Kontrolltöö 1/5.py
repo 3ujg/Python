c009325aa725dd1062e335da436fd2d6c4c1b396
Alar <alar@hkhk.edu.ee>
2025-01-07 09:22:55 +0200

    sadas

---
+++ Kontrolltöö 1/5.py
@@ -0,0 +1,8 @@
+vastus = input("What is the meaning of life, the universe and everything inside it?: ")
+oige = list.append["42", "fourty two", "fourty-two"]
+print(vastus,oige)
+if vastus == oige:
+    print == "Yes!"
+else:
+    print("no...")
+
