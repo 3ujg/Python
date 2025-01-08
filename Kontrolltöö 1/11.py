c0a6353a303d5a5e2d01647b5af7693b186384b8
Alar <alar@hkhk.edu.ee>
2025-01-08 13:02:28 +0200

    FDGS

--- Kontrolltöö 1/11.py
+++ Kontrolltöö 1/11.py
@@ -1,2 +1,13 @@
+tah = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V"]
+num = ["1","2","3","4","5","6","7","8","9","0"]
+
 numbrimark = input("Numbrimärgi kontroll: ")
-isinstance(numbrimark, str)
+if len(numbrimark) > 6 or len(numbrimark) < 6:
+    print("Ei sobi")
+elif numbrimark(len(0,1,2)) == tah and numbrimark[3,4,5] == num:
+    print("Sobib")
+else:
+    print("ei sobi")
+
+
+    
--- Kontrolltöö 1/deep.py
+++ Kontrolltöö 1/deep.py
@@ -1,7 +1,5 @@
-vastus = ["42", "fourty two", "fourty-two", ]
 vastus = input("What is the meaning of life, the universe and everything inside it?: ")
 if vastus in {'42', 'fourty two', 'fourty-two'}:
     print("Yes!")
 else:
     print("no...")
-
