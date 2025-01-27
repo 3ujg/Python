382509be754dd21a8696c6faf50d3d83bc9acc4e
Alar <alar@hkhk.edu.ee>
2025-01-27 13:27:21 +0200

    Json

--- url.py
+++ url.py
@@ -1,18 +1,19 @@
 import requests
 
+otsing = input("Sisesta märksõna: ")
 url = "https://dummy-json.mock.beeceptor.com/comments"
 
 response = requests.get(url)
 
 if response.status_code == 200:
-    data = response.json()
-    comments = data("comments")
-
-comment_id = int(input("Sisesta inimese id: "))
-
-for comment in comments:
-    if comment('id') == comment_id:
-        print(f"User id: {comment_id}")
-        break
-    else:
-        print("Something went wrong.")
+    datas = response.json()
+    for data in datas:
+        if otsing in data['body'] or data['name']:
+            print(f"Postituse ID: {data['postId']}")
+            print(f"Id: {data['id']}")
+            print(f"Name: {data['name']}")
+            print(f"Email: {data['email']}")
+            print(f"Comment body: {data['body']}")
+            break  
+else:
+    print(f"Error: {response.status_code}")
