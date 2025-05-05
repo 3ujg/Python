a01725f5266a70c94ee0bc28efefb2517ab55a2a
Alar <alar@hkhk.edu.ee>
2025-05-05 14:57:02 +0300

    dshsd

--- url.py
+++ url.py
@@ -8,10 +8,9 @@ response = requests.get(url)
 if response.status_code == 200:
     datas = response.json()
     for data in datas:
-        if otsing in data['body'] or data['name']:
-            print(f"Postituse ID: {data['postId']}")
+        if otsing in data['body'] or data['username']:
             print(f"Id: {data['id']}")
-            print(f"Name: {data['name']}")
+            print(f"Name: {data['firstName'and'lastName']}")
             print(f"Email: {data['email']}")
             print(f"Comment body: {data['body']}")
             break  
