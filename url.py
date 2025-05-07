aa30649791f448dc9176f7860975fb80cb9d3419
Alar <alar@hkhk.edu.ee>
2025-05-07 11:34:01 +0300

    sah

--- url.py
+++ url.py
@@ -1,18 +1,19 @@
 import requests
 
 otsing = input("Sisesta märksõna: ")
-url = "https://dummy-json.mock.beeceptor.com/comments"
+url = "https://dummyjson.com/users"
 
 response = requests.get(url)
 
 if response.status_code == 200:
-    datas = response.json()
-    for data in datas:
-        if otsing in data['body'] or data['username']:
-            print(f"Id: {data['id']}")
-            print(f"Name: {data['firstName'and'lastName']}")
-            print(f"Email: {data['email']}")
-            print(f"Comment body: {data['body']}")
-            break  
+    users = response.json()
+    for user in users['users']:
+        if otsing in user['username']:
+            print(f"Id: {user['id']}")
+            print(f"Name: {user['firstName']} {['lastName']}")
+            print(f"Username: {user['username']}")
+            break 
+        else:
+            print("Midagi läks valesti proovi uuesti") 
 else:
     print(f"Error: {response.status_code}")
