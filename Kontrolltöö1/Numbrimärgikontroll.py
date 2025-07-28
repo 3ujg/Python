ce22f75a287ec2c48b3d57a873880ad761714418
Alar <alar@hkhk.edu.ee>
2025-07-29 00:09:27 +0300

    Update Numbrimärgikontroll.py

--- Kontrolltöö1/Numbrimärgikontroll.py
+++ Kontrolltöö1/Numbrimärgikontroll.py
@@ -4,10 +4,8 @@ num = ["1","2","3","4","5","6","7","8","9","0"]
 numbrimark = input("Numbrimärgi kontroll: ")
 if len(numbrimark) > 6 or len(numbrimark) < 6:
     print("Ei sobi")
-if numbrimark[0] and numbrimark[1] and numbrimark[2] in tah:
+elif numbrimark[0] and numbrimark[1] and numbrimark[2] in tah:
         if numbrimark[3]and numbrimark[4]and numbrimark[5] in num:
             print("Sobib")
-else:
-    print("ei Sobi")
-
-    
+        else:
+            print("ei Sobi")
